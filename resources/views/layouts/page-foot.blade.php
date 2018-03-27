dcb7b0cf728a263ea325f5765b55096f94b16dee
DOC <docampbell@gmail.com>
2018-03-27 01:05:21 -0500

    Update sources for js files

--- resources/views/layouts/page-foot.blade.php
+++ resources/views/layouts/page-foot.blade.php
@@ -1,15 +1,15 @@
     <script
-      src="https://code.jquery.com/jquery-3.2.1.js"
+      src="/js/vendor/jquery-3.2.1.js"
       integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE="
       crossorigin="anonymous"></script>
 
     <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="/js/vendor/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-    <script src="https://unpkg.com/vue/dist/vue.js"></script>    
+    <script src="/js/vendor/vue.js"></script>    
     @yield('javascript-controllers-pre')
     <script src="/js/vendor/axios.js"></script>    
     <script src="/js/vendor/moment.js"></script>    
-    <script src="https://unpkg.com/vuetify/dist/vuetify.min.js"></script>
+    <script src="/js/vendor/vuetify.min.js"></script>
     <script src="/js/app.js"></script>
     @yield('javascript-controllers')
